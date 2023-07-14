35f4b28bc0bc97b18e6cdc6757321358a6735d0a
Mark Masoumi <masoumi.mark@outlook.com>
2023-07-13 20:29:23 -0400

    Added compare page with two Datalists which user can select systems from to compare. Added logic to prevent comparing the same console, or consoles they aren't in the DB

---
+++ public/controllers/compare-controller.js
@@ -0,0 +1,54 @@
+// Initialize the submit button to be disabled
+disableSubmit();
+
+// Get list of valid consoles
+const validSystems = getValidSystems();
+
+// Disables submit button
+function disableSubmit(){
+    document.querySelector(".submit-btn").disabled = true;
+}
+
+// Enables submit button
+function enableSubmit(){
+    document.querySelector(".submit-btn").disabled = false;
+}
+
+// Returns system 1 selection
+function system1Selection(){
+    return document.querySelector(".system1").value;
+}
+
+// Returns system 2 selection
+function system2Selection(){
+    return document.querySelector(".system2").value; 
+}
+
+// Returns true if chosen System is in system list otherwise returns false
+function validateSelection(systemName){
+    return validSystems.includes(systemName);
+}
+
+// Called when either Datalist value is changed
+// Checks that both selections are valid and not the same
+// If so, enables submit button, otherwise disables it
+function checkSelections(){    
+    if(validateSelection(system1Selection()) && validateSelection(system2Selection()) && system1Selection() !== system2Selection()){
+        enableSubmit();
+    }
+    else{
+        disableSubmit();
+    }
+}
+
+// Returns an array of all valid system names from the Datalist
+function getValidSystems() {
+    const validSystems = [];
+    const systems = document.querySelector(".datalist1");  
+    for (let i = 0; i < systems.options.length; i++) {
+        validSystems.push(systems.options[i].value);
+    }
+    return validSystems;
+}
+
+
--- server/systems.js
+++ server/systems.js
@@ -7,25 +7,65 @@ const router = express.Router();
 // Requre in the requests module
 const requests = require("../services/requests.js");
 
-// GET systems (/systems)
-router.get("/systems", async (req, res) => {
+// Middleware to select all systems and add them to the request parameter
+// for routes that begin with '/systems'
+router.use('/systems', async (req, res, next) => {
     try {
         // Get all systems from the DB
         const resultArray = await requests.getAllSystems();
 
         // Store only the dataValue objects in a systems array
-        // (dataValue objects are single objects that contain the data of one system)
+        // (dataValue objects are single objects that contain the data for one system)
         const systems = [];
         for (element in resultArray) {
             systems.push(resultArray[element].dataValues);
         }
 
-        // Render the systems page using systems data
-        res.status(200).render("systems", { systems: systems });
+        // Add the systems array to the request parameter
+        req.systems = systems;
+        next();
     } catch (err) {
         res.status(500).send(err);
     }
 });
+
+// GET systems (/systems)
+router.get("/systems", async (req, res) => {
+    // Render the systems page using systems data
+    res.status(200).render("systems", { systems: req.systems });
+});
+
+// GET Compare (/systems/compare)
+router.get("/systems/compare", async (req, res) => {
+    // Render the compare page using systems data
+    res.status(200).render("compare", { systems: req.systems });
+});
+
+// GET VS (systems/vs)
+router.get("/systems/vs", async (req, res) => {
+    // If either system1 or system2 aren't included in the URL, redirect user back to compare page
+    if (!req.query.system1 || !req.query.system2) {
+        res.redirect("/systems/compare");
+    }
+    else {
+        // Check that both req.query.system(1 & 2) parameters are valid
+        const systemNames = [];
+        for (element in req.systems) {
+            systemNames.push(req.systems[element].name);
+        }
+
+        if (!systemNames.includes(req.query.system1) || !systemNames.includes(req.query.system2)) {
+            res.redirect("/systems/compare");
+        }
+        else {
+            // Render the VS page using the two systems data
+            res.status(200).render("vs");
+        }
+    }
+});
+
+// TODO: Add a systems/compare/:id route from a system page
+// To take user to the compare page with one of the systems pre-selected
 
 // GET system (/systems/:systemId)
 router.get("/systems/:id", async (req, res) => {
